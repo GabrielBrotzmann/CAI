a044d3bd59ed76ea854994af1f818548c1703475
gabrielbrotzmann <gabrielbrotzmann@live.com.ar>
2021-03-17 23:57:22 -0300

    Agregar archivos de proyecto.

---
+++ A890222.Actividad01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A890222.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
